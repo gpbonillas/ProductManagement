ce85d3340988e1bac7cad91ac24168e243ff4d18
Patricio Bonilla <gpbonillas@gmail.com>
2021-03-27 02:22:44 -0500

    Commit inicial. Lesson 4 finished.

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2021 gpbonillas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
+/**
+ * {@code Shop} class represents an application that manages Products
+ * @version 4.0
+ * @author gpbonillas
+ */
+public class Shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Product p1 = new Product();
+        
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
+    }
+    
+}
---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 gpbonillas
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
+
+/**
+ * {@code Product} class represents properties and behaviours of
+ * product objects in the Product Management System.
+ * <br>
+ * Each product has an id, name, and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ * @version 4.0
+ * @author gpbonillas
+ */
+public class Product {
+    
+    private int id;
+    private String name;
+    private BigDecimal price;
+    
+    /**
+     * A constant that defines a 
+     * {@link java.math.BigDecimal BigDecimal} value of the discount rate
+     * <br>
+     * Discount rate is 10%
+     */
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(final BigDecimal price) {
+//        price = BigDecimal.ONE;
+        this.price = price;
+    }
+    
+    /**
+     * Calculates discount based on a product price and 
+     * {@link DISCOUNT_RATE discount rate}
+     * 
+     * @return a {@link java.math.BigDecimal BigDecimal} value of the discount
+     */
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+}
