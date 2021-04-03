0fe1721a4f9b4e40e7f2c3149c6a9474d639906f
Patricio Bonilla <gpbonillas@gmail.com>
2021-04-03 18:31:45 -0500

    Test the Product Review Functionality Practice 7-3

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -40,34 +40,29 @@ public class Shop {
         
         ProductManager pm = new ProductManager(Locale.UK);
         
-        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
+        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);        
+        pm.printProductReport();       
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        pm.printProductReport();
         
-        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
-        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-        //((Food) p3).getBestBefore();
-        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
-        Product p5 = p3.applyRating(Rating.THREE_STAR);
-        
-        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99),  Rating.FIVE_STAR);
-        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99),  Rating.FIVE_STAR, LocalDate.now().plusDays(2));
-        
-        Product p8 = p4.applyRating(Rating.FIVE_STAR);
-        
-        Product p9 = p1.applyRating(Rating.TWO_STAR);
-        
-        System.out.println(p6.equals(p7));
-        
-
-        System.out.println(p1.toString());
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        
-        System.out.println(p6);
-        System.out.println(p7);
-        System.out.println(p8);
-        System.out.println(p9);
+//        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+//        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));      
+//        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
+//        Product p5 = p3.applyRating(Rating.THREE_STAR);       
+//        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99),  Rating.FIVE_STAR);
+//        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99),  Rating.FIVE_STAR, LocalDate.now().plusDays(2));       
+//        Product p8 = p4.applyRating(Rating.FIVE_STAR);       
+//        Product p9 = p1.applyRating(Rating.TWO_STAR);       
+//        System.out.println(p6.equals(p7));        
+//        System.out.println(p1.toString());
+//        System.out.println(p2);
+//        System.out.println(p3);
+//        System.out.println(p4);
+//        System.out.println(p5);        
+//        System.out.println(p6);
+//        System.out.println(p7);
+//        System.out.println(p8);
+//        System.out.println(p9);
         
     }
     
