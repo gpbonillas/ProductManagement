19ea5e602807bbadcd3c018bd0fd02ec3ef4f88d
Patricio Bonilla <gpbonillas@gmail.com>
2021-04-03 18:25:51 -0500

    Enable Products Review and Rating Practice 7-2

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -18,6 +18,7 @@ package labs.pm.app;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Locale;
 import labs.pm.data.Drink;
 import labs.pm.data.Food;
 import labs.pm.data.Product;
@@ -37,7 +38,7 @@ public class Shop {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        ProductManager pm = new ProductManager();
+        ProductManager pm = new ProductManager(Locale.UK);
         
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
         
--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -17,18 +17,70 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 /**
  *
  * @author gpbonillas
  */
 public class ProductManager {
-    public Product createProduct (int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
-        return new Food(id, name, price, rating, bestBefore);
+
+    private Product product;
+    private Review review;
+
+    private Locale locale;
+    private ResourceBundle resources;
+    private DateTimeFormatter dateFormat;
+    private NumberFormat moneyFormat;
+
+    public ProductManager(Locale locale) {
+        this.locale = locale;
+        resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
+        dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
+        moneyFormat = NumberFormat.getCurrencyInstance(locale);
     }
-    
-    public Product createProduct (int id, String name, BigDecimal price, Rating rating) {
-        return new Drink(id, name, price, rating);
+
+    public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        product = new Food(id, name, price, rating, bestBefore);
+        return product;
+    }
+
+    public Product createProduct(int id, String name, BigDecimal price, Rating rating) {
+        product = new Drink(id, name, price, rating);
+        return product;
+    }
+
+    public Product reviewProduct(Product product, Rating rating, String comments) {
+        review = new Review(rating, comments);
+        this.product = product.applyRating(rating);
+        return this.product;
+    }
+
+    public void printProductReport() {
+        StringBuilder txt = new StringBuilder();
+        txt.append(MessageFormat.format(resources.getString("product"),
+                product.getName(),
+                moneyFormat.format(product.getPrice()),
+                product.getRating().getStars(),
+                dateFormat.format(product.getBestBefore())));
+
+        txt.append('\n');
+
+        if (review != null) {
+            txt.append(MessageFormat.format(resources.getString("review"),
+                    review.getRating().getStars(),
+                    review.getComments()));
+        } else {
+            txt.append(resources.getString("no.reviews"));
+        }
+        
+        txt.append('\n');
+        System.out.println(txt);
     }
 }
