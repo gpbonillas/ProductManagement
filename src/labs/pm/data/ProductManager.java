33cf779f3dad614ed1d05c9e6801a84ea685c302
Patricio Bonilla <gpbonillas@gmail.com>
2021-04-10 00:55:55 -0500

    Produce Customized Product Reports. Practice 10-2

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -25,6 +25,7 @@ import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -135,6 +136,18 @@ public class ProductManager {
             txt.append('\n');
         }
 
+        System.out.println(txt);
+    }
+    
+    public void printProducts(Comparator<Product> sorter) {
+        List<Product> productList = new ArrayList<>(products.keySet());
+        productList.sort(sorter);
+        StringBuilder txt = new StringBuilder();
+        
+        for(Product product : productList) {
+            txt.append(formatter.formatProduct(product));
+            txt.append('\n');
+        }
         System.out.println(txt);
     }
 
